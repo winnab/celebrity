5441b917052e1219fd242359006753eeefc6c65d
Winna Bridgewater <winnab@gmail.com>
2014-10-23 21:02:32 +0100

    Update players to have clues and check for minimum number of clues before starting

--- lib/game.rb
+++ lib/game.rb
@@ -4,13 +4,12 @@ module Game
 
   class Play
     attr_accessor :players, :num_teams, :teams
+    MIN_NUM_PLAYERS = 6
 
     def initialize players, num_teams = 2
       @players = players
       @num_teams = num_teams
-      puts "about to assign teams"
       @teams = create_teams players, num_teams
-      puts "teams: #{@teams}"
     end
 
     def create_teams players, num_teams
@@ -19,6 +18,27 @@ module Game
       players_per_team = (players.length / num_teams).floor
       players.each_slice(players_per_team) { | p | teams << p }
       teams
+    end
+
+    def can_start?
+      has_players? && has_clues? && sufficient_players? && sufficient_clues?
+    end
+
+    def has_players?
+      @players.all? { | p | p[:name] }
+    end
+
+    def has_clues?
+      @players.all? { | p | p[:clues] }
+    end
+
+    def sufficient_players?
+      @players.length >= MIN_NUM_PLAYERS
+    end
+
+    def sufficient_clues?
+      total = @players.reduce(0) { | ret, p | ret += p[:clues].length }
+      total >= ( @players.length * 4 )
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,17 @@
 require "game"
+require "pry"
 describe Game do
   describe Game::Play do
+    let(:playersObj) {
+      ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"].map do | p |
+        { name: p, clues: 5.times.each_with_object([]) { | i, obj | obj << "clue-#{i}"}  }
+      end
+     }
+    let(:num_teams) { 2 }
+    let(:game) { Game::Play.new(playersObj, num_teams) }
+
+     # check that game.new checks that the player obj is passed in with name and clues
     context "having six or more players" do
-      let(:players) { ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"] }
-      let(:num_teams) { 2 }
-      let(:game) { Game::Play.new(players, num_teams) }
       it "should create teams" do
         expect(game).to respond_to(:teams)
       end
@@ -13,6 +20,32 @@ describe Game do
         min_size, max_size = game.teams.map(&:length).minmax
         expect(max_size - min_size).to be <= 1
       end
+
+      it "can be started" do
+        expect(game.can_start?).to be true
+      end
     end
+
+    context "invalid game" do
+      context "having fewer than 6 players" do
+        let(:game) { Game::Play.new(playersObj[0, 4], num_teams) }
+
+        it "cannot be started" do
+          expect(game.can_start?).to be false
+        end
+      end
+      context "having an insufficient number of clues" do
+        let(:playersObj) {
+          ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"].map do | p |
+            { name: p, clues: 3.times.each_with_object([]) { | i, obj | obj << "clue-#{i}"}  }
+          end
+         }
+
+        it "cannot be started", focus:true do
+          expect(game.can_start?).to be false
+        end
+      end
+    end
+
   end
 end
