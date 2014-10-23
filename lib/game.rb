f91abbb8b1dab0bdd1f1bb5c1cf00350703a0704
Winna Bridgewater <winnab@gmail.com>
2014-10-23 19:45:26 +0100

    Check team distribution

--- lib/game.rb
+++ lib/game.rb
@@ -4,18 +4,21 @@ module Game
 
   class Play
     attr_accessor :players, :num_teams, :teams
+
     def initialize players, num_teams = 2
       @players = players
       @num_teams = num_teams
+      puts "about to assign teams"
+      @teams = create_teams players, num_teams
+      puts "teams: #{@teams}"
     end
 
     def create_teams players, num_teams
-      @teams = []
-      players_per_team = Math.floor(players / num_teams)
-      players.shuffle
-      num_teams.times do | assign |
-        @teams << players
-      end
+      teams = []
+      players.shuffle!
+      players_per_team = (players.length / num_teams).floor
+      players.each_slice(players_per_team) { | p | teams << p }
+      teams
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,17 @@
 require "game"
-
 describe Game do
   describe Game::Play do
     context "having six or more players" do
-      let(:players) { ["winna", "neil", "chucky", "jessica", "richa", "deb"] }
+      let(:players) { ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"] }
       let(:num_teams) { 2 }
       let(:game) { Game::Play.new(players, num_teams) }
       it "should create teams" do
-        game.should respond_to(:teams)
+        expect(game).to respond_to(:teams)
+      end
 
+      it "should assign players to teams evenly" do
+        min_size, max_size = game.teams.map(&:length).minmax
+        expect(max_size - min_size).to be <= 1
       end
     end
   end
