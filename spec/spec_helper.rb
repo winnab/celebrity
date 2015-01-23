5c3a9a2d251a7a0e6dc337fcb8ef77fd7edc20cb
Winna Bridgewater <winnab@gmail.com>
2015-01-23 10:37:33 +0000

    Prune client-side code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,4 @@
-require_relative "../controllers/main"
-require_relative "../controllers/invite"
-require_relative "../controllers/game"
-require_relative "../controllers/round"
-require_relative "../controllers/turn"
-require_relative "../controllers/player"
-require_relative "../controllers/team"
-
-require "pry"
-
-# Configure RSpec
 RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
 
