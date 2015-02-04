532222961f883edd2658c8c5f8b706659697970b
Winna Bridgewater <winnab@gmail.com>
2015-02-04 12:25:49 +0000

    Set up Pony for emails, add Pry for debugging, add example .env

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "pony"
 
 group :test do
   gem "capybara"
--- celebrity_app.rb
+++ celebrity_app.rb
@@ -6,8 +6,23 @@ rescue LoadError
   require "sinatra/contrib/all"
 end
 
+require "pony"
+
 enable :sessions
-set :session_secret, '*&(^T314' # from here: https://stackoverflow.com/questions/18044627/sinatra-1-4-3-use-racksessioncookie-warning
+set :session_secret, ENV["SESSION_KEY"]
+
+Pony.options = {
+  from: "winnab@gmail.com",
+  via: :smtp,
+  via_options: {
+    address: "smtp.mandrillapp.com",
+    port: "25",
+    enable_starttls_auto: true,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_APIKEY"],
+    authentication: "plain"
+  }
+}
 
 get "/" do
   erb :index
@@ -24,6 +39,17 @@ get_or_post "/game_overview" do
   @name = session["creator_name"]
   @players = []
   @players << params["invite-email"] unless params["invite-email"].nil?
+post "/invite" do
+  @players = []
+  if params["invite-email"].size > 0
+    @players << params["invite-email"]
+    Pony.mail(
+      to: params["invite-email"],
+      from: Pony.options.from,
+      subject: "Join my game!",
+      body: "Hey this is a link!"
+    )
+  end
   erb :game_overview
 end
 
