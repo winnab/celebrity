66b7e3560e809e743fcef193b133ecca77c05d0f
Winna Bridgewater <winnab@gmail.com>
2015-02-06 04:25:02 +0000

    Add pending steps for accepting an invite

--- features/step_definitions/invite_players_steps.rb
+++ features/step_definitions/invite_players_steps.rb
@@ -17,3 +17,19 @@ end
 And(/^the player has received an email invitation$/) do
   expect(Pony).to have_received(:mail).with(to: "bob@example.com")
 end
+
+Given(/^I've been invited to a game$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should have received an email invitation$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it has a nice welcome message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a link to the game$/) do
+  pending # express the regexp above with the code you wish you had
+end
