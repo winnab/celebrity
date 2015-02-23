75cacd9417aa24cd79365d54856ae6033edd9725
winnab <winnab@gmail.com>
2015-02-23 17:21:36 +0000

    Require both  name and email address for creating a game

--- features/create_game.feature
+++ features/create_game.feature
@@ -2,5 +2,6 @@ Feature: Create a Game
   Scenario: Create a game on the homepage
     Given I am on the homepage
     And   I input my name
+    And   I input my email
     And   I click the Create Game button
     Then  I see the new game with myself as the only user
--- features/step_definitions/create_game_steps.rb
+++ features/step_definitions/create_game_steps.rb
@@ -6,6 +6,10 @@ When(/^I input my name$/) do
   fill_in("creator_name", :with => "Winna")
 end
 
+And(/^I input my email$/) do
+  fill_in("creator_email", :with => "test@example.com")
+end
+
 When(/^I click the Create Game button$/) do
   click_button "Create Game"
 end
