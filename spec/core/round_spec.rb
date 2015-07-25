3d298bfe7df4bca6a358e354bddff87c3d326d0b
Winna Bridgewater <winnab@gmail.com>
2015-07-25 12:40:11 +0100

    Fix round spec

--- spec/core/round_spec.rb
+++ spec/core/round_spec.rb
@@ -3,43 +3,50 @@ require_relative "../../app/core/player"
 require_relative "../../app/core/team"
 require_relative "../../app/core/round"
 require_relative "../../app/core/turn"
+require_relative "../../app/services/game_store"
+require_relative "../../app/services/player_store"
+
 require "spec_helper"
 
 describe Round do
-  let(:players) {
-    ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"]
-  }
+  before do
+    game_store = GameStore.new
+    game_store.add(Game.new)
+    @game = game_store.list.last
+    num_teams = 2
 
-  let(:players_obj) {
-    players.map do | p |
-      {
-        name: p,
-        clues: 5.times.each_with_object([]) { | i, obj | obj << "#{p}-clue-#{i}" }
-      }
+    player_store = PlayerStore.new
+    player_names = ["winna", "neil", "chucky", "jessica", "richa", "deb", "divya", "gautam"]
+
+    player_names.each do |name|
+      player_store.add(Player.new(@game.id, name))
     end
-   }
 
-  let(:num_teams) { 2 }
+    player_names.each do | name |
+      5.times.each_with_object([]) do | i, obj |
+        clues = obj << "#{name}-clue-#{i}"
+        game_store.add_clues_to_game(@game.id, clues)
+      end
+    end
 
-  let(:game) { Game.new(players_obj.first).start(players_obj, num_teams) }
-
-  let(:round) { game.current_round }
+    @game.start(player_store.find_all_by_game_id(@game.id), num_teams)
+  end
 
   it "#new_turn" do
-    expect(round.new_turn).to be_a(Turn)
+    expect(@game.current_round.new_turn).to be_a(Turn)
   end
 
   describe "#next_player" do
-    before(:each) { game.set_player_lineup }
+    before(:each) { @game.set_player_lineup }
 
     it "picks the next player in the lineup" do
-      expect(game.next_player).to_not be(game.current_player)
-      expect(game.next_player).to be(game.lineup[1])
+      expect(@game.next_player).to_not be(@game.current_player)
+      expect(@game.next_player).to be(@game.lineup[1])
     end
 
     it "loops back to the start of the lineup when needed" do
-      game.current_player_ix = game.lineup.length - 1
-      expect(game.next_player).to be(game.lineup[0])
+      @game.current_player_ix = @game.lineup.length - 1
+      expect(@game.next_player).to be(@game.lineup[0])
     end
   end
 
